52efaabd1301f3c48dbb46a129ec7e475eae3c21
Gallay David <david.gallay@heig-vd.ch>
2020-03-02 15:38:36 +0100

    define class

--- src/date.h
+++ src/date.h
@@ -1,9 +1,19 @@
 #ifndef DATE_H
 #define DATE_H
 
+enum class Month {
+
+};
+
 class Date {
+    public:
+        Date(unsigned day, unsigned month, unsigned year);
+        Date(unsigned day, Month month, unsigned year);
 
-
+    private:
+        unsigned _year;
+        unsigned _month;
+        unsigned _day;
 };
 
 #endif
