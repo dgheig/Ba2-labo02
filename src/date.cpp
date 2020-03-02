a12f25798839676c766cf19f99158c5a4b3611f6
Otabiel <brunoc347@gmail.com>
2020-03-02 16:27:53 +0100

    Commencé la préparation des operateur

--- src/date.cpp
+++ src/date.cpp
@@ -1,1 +1,69 @@
 #include "date.h"
+
+std::ostream& operator<<(std::ostream& os, const Date& date){
+    return date.afficher(os);
+}
+std::ostream & Date::afficher(std::ostream &os) const{
+    return os << _day  << "." << _month << "." << _year;
+}
+
+bool Date::operator==(const Date &date) const {
+    if(this->_year == date._year){
+        if(this->_month == date._month){
+            if(this->_day == date._day){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Date::operator!=(const Date &date) const {
+    if(this->_year != date._year){
+        if(this->_month != date._month){
+            if(this->_day != date._day){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Date::operator<(const Date &date) const {
+    if(this->_year < date._year){
+        if(this->_month < date._month){
+            if(this->_day < date._day){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Date::operator>(const Date &date) const {
+    if(this->_year > date._year){
+        if(this->_month > date._month){
+            if(this->_day > date._day){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Date::operator<=(const Date &date) const {
+    return *this == date or *this < date;
+}
+
+bool Date::operator>=(const Date &date) const {
+    return *this == date or *this > date;
+}
+
+Date Date::operator+(int jours) const {
+
+}
+
+Date Date::operator-(int jours) const {
+
+}
+
--- src/date.h
+++ src/date.h
@@ -1,6 +1,8 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+
 enum class Month {
 
 };
@@ -10,10 +12,24 @@ class Date {
         Date(unsigned day, unsigned month, unsigned year);
         Date(unsigned day, Month month, unsigned year);
 
+        bool operator==(const Date& date) const;
+        bool operator!=(const Date& date) const;
+        bool operator<(const Date& date) const;
+        bool operator<=(const Date& date) const;
+        bool operator>(const Date& date) const;
+        bool operator>=(const Date& date) const;
+        Date operator+(int jours) const;
+        Date operator-(int jours) const;
+        std::ostream& afficher(std::ostream& os = std::cout) const;
+
     private:
         unsigned _year;
         unsigned _month;
         unsigned _day;
+
 };
+
+std::ostream& operator<<(std::ostream& os, const Date& date);
+
 
 #endif
