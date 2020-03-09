e7d9fe4b056c936f1deecba97dad031df27d4658
Otabiel <brunoc347@gmail.com>
2020-03-09 16:21:44 +0100

    Ajout des commentaires doxygen pour le reste des fonctions

--- labo_01_bruno_carvalho_gallay_david.cpp
+++ labo_01_bruno_carvalho_gallay_david.cpp
@@ -21,23 +21,104 @@ using namespace std;
 #define WAIT_ENTER while(cin.get()!='\n')
 
 int main() {
-
-    Date date(12,3,1980);
-    cout << date << endl;
-    Date date2 = date + 40;
-    cout << date2.isValid() << endl;
-    cout << date2 << endl;
-
-    date = Date(1, 1, 2020);
-    cout << date + 1 << endl;
-    cout << date + 30 << endl;
-    cout << date + (-31) << endl;
-
-    date = Date(1, 2, 2020);
-    cout << date - 1 << endl;
-    cout << date - 30 << endl;
-    cout << date - 31 << endl;
-
+    //TEST INPUT STREAM //
+    {
+        Date date;
+    }
+    cout << "TEST '=' OPERATOR" << endl;
+    {
+        Date date1(12,1,1990);
+        cout << date1 << endl;
+        Date date2 = date1;
+        cout << date2 << endl;
+    }
+    cout << "TEST '<' OPERATOR" << endl;
+    {
+        Date date1(12,1,1990);
+        Date date2(13, 3, 2000);
+        cout << date1 << "<" << date2 << " : ";
+        cout << boolalpha << (date1 < date2) << endl;
+        cout << date2 << "<" << date1 << " : ";
+        cout << boolalpha << (date2 < date1) << endl;
+    }
+    cout << "TEST '<=' OPERATOR" << endl;
+    {
+        Date date1(14,3,2002);
+        Date date2(1,5,1980);
+        cout << date1 << "<=" << date2 << " : ";
+        cout << boolalpha << (date1 <= date2) << endl;
+        cout << date2 << "<=" << date1 << " : ";
+        cout << boolalpha << (date2 <= date1) << endl;
+    }
+    cout << "TEST '>' OPERATOR" << endl;
+    {
+        Date date1(12,1,1990);
+        Date date2(13, 3, 2000);
+        cout << date1 << ">" << date2 << " : ";
+        cout << boolalpha << (date1 > date2) << endl;
+        cout << date2 << ">" << date1 << " : ";
+        cout << boolalpha << (date2 > date1) << endl;
+    }
+    cout << "TEST '>=' OPERATOR" << endl;
+    {
+        Date date1(14,3,2002);
+        Date date2(1,5,1980);
+        cout << date1 << ">=" << date2 << " : ";
+        cout << boolalpha << (date1 >= date2) << endl;
+        cout << date2 << ">=" << date1 << " : ";
+        cout << boolalpha << (date2 >= date1) << endl;
+    }
+    cout << "TEST '==' OPERATOR" << endl;
+    {
+        Date date1(14,5,2000);
+        Date date2(14,5,2000);
+        Date date3(15,5,2000);
+        cout << date1 << "==" << date2 << " : ";
+        cout << boolalpha << (date1 == date2) << endl;
+        cout << date1 << "==" << date3 << " : ";
+        cout << boolalpha << (date1 == date3) << endl;
+    }
+    cout << "TEST '!=' OPERATOR" << endl;
+    {
+        Date date1(14,5,2000);
+        Date date2(14,5,2000);
+        Date date3(15,5,2000);
+        cout << date1 << "!=" << date2 << " : ";
+        cout << boolalpha << (date1 != date2) << endl;
+        cout << date1 << "!=" << date3 << " : ";
+        cout << boolalpha << (date1 != date3) << endl;
+    }
+    cout << "TEST '+' OPERATOR" << endl;
+    {
+        Date date1;
+        cout << date1 << "+" << "7" << " = ";
+        cout << (date1 + 7) << endl;
+    }
+    cout << "TEST '-' OPERATOR" << endl;
+    {
+        Date date1;
+        cout << date1 << "-" << "22" << " = ";
+        cout << (date1 - 22) << endl;
+    }
+    cout << "TEST '++' OPERATOR" << endl;
+    {
+        Date date1(15,1,1997);
+        cout << "Post-increment " << date1++ << endl;
+        cout << "After Post-increment " << date1 << endl;
+        cout << "Pre-increment " << ++date1 << endl;
+    }
+    cout << "TEST '--' OPERATOR" << endl;
+    {
+        Date date1(15,1,1997);
+        cout << "Post-decrement " << date1-- << endl;
+        cout << "After Post-decrement " << date1 << endl;
+        cout << "Pre-decrement " << --date1 << endl;
+    }
+    cout << "TEST 'string()' CAST OPERATOR" << endl;
+    {
+        Date date(23,8,2007);
+        cout << string(date) << endl;
+    }
 
     cout << "Please, press <ENTER> to end the program" << endl;
     WAIT_ENTER;
